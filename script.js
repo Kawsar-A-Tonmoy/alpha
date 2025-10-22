397990da99dea4ff674c9e22cbce2cb331726d43
Kawsar-A-Tonmoy <kawsar.4hm3d@gmail.com>
2025-10-22 17:17:41 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<DOCUMENT filename="script.js">
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js';
 import { getFirestore, collection, getDocs, addDoc, doc, updateDoc, deleteDoc, getDoc, orderBy, query, where, runTransaction } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
@@ -826,4 +826,3 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
   }
 });
-</DOCUMENT>
